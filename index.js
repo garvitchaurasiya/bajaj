00420c7177d9cfe989ad0e9874af3987527cb114
garvitchaurasiya <74445186+garvitchaurasiya@users.noreply.github.com>
2023-03-29 11:11:58 +0000

    Committed

--- index.js
+++ index.js
@@ -1,47 +1,65 @@
-const express = require('express')
-var cors = require('cors')
+const app = require('express')();
+const { v4 } = require('uuid');
 
-const app = express()
-const port = process.env.PORT || 5000
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-app.use(cors())
-app.use(express.json());
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
-app.post('/', (req, res)=>{
-    // let data = req.body.data;
+module.exports = app;
 
-    // let output = {
-    //     'is_success': true,
-    //     'user_id': 'garvit_chaurasiya_25072002',
-    //     'email': 'garvit1146@chitkara.edu.in',
-    //     'roll_number': [],
-    //     'odd_numbers': [],
-    //     'even_numbers': [],
-    //     'alphabets': []
-    // }
 
-    // for(var i = 0; i<data.length; i++){
-    //     var x = data[i];
-    //     if(x[0]>='1' && x[0]<='9'){
-    //         var num = x - 0;
-    //         if(num % 2 == 0){
-    //             output.even_numbers.push(x);
-    //         }
-    //         else{
-    //             output.odd_numbers.push(x);
-    //         }
-    //     }
-    //     else{
-    //         output.alphabets.push(x.toUpperCase());
-    //     }
-    // }
+// const express = require('express')
+// var cors = require('cors')
 
-    // res.send(output);
+// const app = express()
+// const port = process.env.PORT || 5000
 
-    res.send("working");
+// app.use(cors())
+// app.use(express.json());
 
-})
+// app.post('/', (req, res)=>{
+//     // let data = req.body.data;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+//     // let output = {
+//     //     'is_success': true,
+//     //     'user_id': 'garvit_chaurasiya_25072002',
+//     //     'email': 'garvit1146@chitkara.edu.in',
+//     //     'roll_number': [],
+//     //     'odd_numbers': [],
+//     //     'even_numbers': [],
+//     //     'alphabets': []
+//     // }
+
+//     // for(var i = 0; i<data.length; i++){
+//     //     var x = data[i];
+//     //     if(x[0]>='1' && x[0]<='9'){
+//     //         var num = x - 0;
+//     //         if(num % 2 == 0){
+//     //             output.even_numbers.push(x);
+//     //         }
+//     //         else{
+//     //             output.odd_numbers.push(x);
+//     //         }
+//     //     }
+//     //     else{
+//     //         output.alphabets.push(x.toUpperCase());
+//     //     }
+//     // }
+
+//     // res.send(output);
+
+//     res.send("working");
+
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
