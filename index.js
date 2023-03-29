784bb64755ca07079f334f4fbf48b945355c2bc2
garvitchaurasiya <74445186+garvitchaurasiya@users.noreply.github.com>
2023-03-29 11:05:28 +0000

    Committed

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+const express = require('express')
+var cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json());
 
 app.post('/', (req, res)=>{
-    let data = req.body.data;
+    // let data = req.body.data;
 
-    let output = {
-        'is_success': true,
-        'user_id': 'garvit_chaurasiya_25072002',
-        'email': 'garvit1146@chitkara.edu.in',
-        'roll_number': [],
-        'odd_numbers': [],
-        'even_numbers': [],
-        'alphabets': []
-    }
+    // let output = {
+    //     'is_success': true,
+    //     'user_id': 'garvit_chaurasiya_25072002',
+    //     'email': 'garvit1146@chitkara.edu.in',
+    //     'roll_number': [],
+    //     'odd_numbers': [],
+    //     'even_numbers': [],
+    //     'alphabets': []
+    // }
 
-    for(var i = 0; i<data.length; i++){
-        var x = data[i];
-        if(x[0]>='1' && x[0]<='9'){
-            var num = x - 0;
-            if(num % 2 == 0){
-                output.even_numbers.push(x);
-            }
-            else{
-                output.odd_numbers.push(x);
-            }
-        }
-        else{
-            output.alphabets.push(x.toUpperCase());
-        }
-    }
+    // for(var i = 0; i<data.length; i++){
+    //     var x = data[i];
+    //     if(x[0]>='1' && x[0]<='9'){
+    //         var num = x - 0;
+    //         if(num % 2 == 0){
+    //             output.even_numbers.push(x);
+    //         }
+    //         else{
+    //             output.odd_numbers.push(x);
+    //         }
+    //     }
+    //     else{
+    //         output.alphabets.push(x.toUpperCase());
+    //     }
+    // }
 
-    res.send(output);
+    // res.send(output);
+
+    res.send("working");
 
 })
 
-app.listen(port, ()=>{
-    console.log("Listing on port");
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
